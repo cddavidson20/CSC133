c8ab4a9a50fdcb1ec340188f997bf7dfe845e7be
Ronny Ritprasert <ronnyrit@gmail.com>
2020-03-31 18:31:34 -0700

    Added temporary pathing

--- TowerDefense/app/src/main/java/csc133/towerdefense/TowerGame.java
+++ TowerDefense/app/src/main/java/csc133/towerdefense/TowerGame.java
@@ -5,7 +5,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.PointF;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
