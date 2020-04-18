565efd90924a7be957b30a9a29e5d2cd6c1a6619
Ronny Ritprasert <ronnyrit@gmail.com>
2020-04-18 02:14:31 -0700

    attempted wave creation

--- TowerDefense/app/src/main/java/csc133/towerdefense/GameObjects/GameObject.java
+++ TowerDefense/app/src/main/java/csc133/towerdefense/GameObjects/GameObject.java
@@ -8,9 +8,11 @@ import android.view.SurfaceView;
 
 import java.util.ArrayList;
 
+import csc133.towerdefense.GameObjects.enemies.AbstractEnemy;
 import csc133.towerdefense.GameObjects.enemies.Enemy;
 import csc133.towerdefense.GameObjects.towers.AbstractTower;
 import csc133.towerdefense.GameObjects.towers.DefenseTower;
+import csc133.towerdefense.Path;
 
 public class GameObject extends SurfaceView {
 
@@ -19,6 +21,8 @@ public class GameObject extends SurfaceView {
 
     private Enemy enemy;
     private ArrayList<AbstractTower> towers;
+    private ArrayList<AbstractEnemy> enemies;
+    private Path path;
 
     private final int NUM_BLOCKS_WIDE = 40;
     private int mNumBlocksHigh;
@@ -34,7 +38,8 @@ public class GameObject extends SurfaceView {
         mNumBlocksHigh = size.y / blockSize;
 
         towers = new ArrayList<>();
-
+        enemies = new ArrayList<>();
+        path = new Path(blockSize);
         enemy = new Enemy(context, blockSize, new Point(50, 50));
     }
 
@@ -45,17 +50,36 @@ public class GameObject extends SurfaceView {
 
     public void draw(Canvas mCanvas, Paint mPaint) {
         enemy.draw(mCanvas, mPaint);
+
+        for (AbstractEnemy e : enemies) {
+            e.draw(mCanvas, mPaint);
+        }
+
         for (AbstractTower t : towers) {
             t.draw(mCanvas, mPaint);
         }
+
+        path.drawPath(mCanvas, mPaint);
     }
 
+    // I don't think this is where I should be looping spawn enemy.
     public void update() {
         enemy.move();
+        spawnEnemy();
+
+        for (AbstractEnemy e : enemies) {
+            e.move();
+        }
+    }
+
+    // I see that newTowerLocation creates a tower and adds it to the array,
+    // so I'm trying something like that with spawnEnemy.
+    public void spawnEnemy() {
+        enemies.add(new Enemy(context, blockSize, new Point(50, 50)));
     }
 
     public void newTowerLocation(int x, int y) {
-       towers.add(new DefenseTower(context, new Point(x, y),enemy, blockSize));
+       towers.add(new DefenseTower(context, new Point(x, y), enemy, blockSize));
     }
 
 }
--- TowerDefense/app/src/main/java/csc133/towerdefense/GameObjects/enemies/AbstractEnemy.java
+++ TowerDefense/app/src/main/java/csc133/towerdefense/GameObjects/enemies/AbstractEnemy.java
@@ -1,7 +1,15 @@
 package csc133.towerdefense.GameObjects.enemies;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 public abstract class AbstractEnemy {
 
     public AbstractEnemy() {}
 
+    public void draw(Canvas mCanvas, Paint mPaint) {}
+
+    public void move() {
+
+    }
 }
