bd880e745e40babed237ac3f3b5229db03012a4e
Ronnapob Ritprasert <ronnyrit@gmail.com>
2020-05-11 01:08:34 -0700

    asdffdsfdsfsd

---
+++ TowerDefense/app/src/main/java/csc133/towerdefense/game/engine/interfaces/IUpdateComponent.java
@@ -0,0 +1,15 @@
+package csc133.towerdefense.game.engine.interfaces;
+
+import csc133.towerdefense.game.engine.Transform;
+
+public interface IUpdateComponent {
+
+    // Everything has one of these so it can be updated
+    // The player's transform could be left out because it is only needed
+    // by the background(to see which way the player is moving)
+    // However, if we added enemies at a later date they would likely need
+    // to know where the player is and what he is doing.
+    // So I left it in rather than create a communication between the
+    // background and the player
+    void update(long fps, Transform t, Transform playerTransform);
+}
